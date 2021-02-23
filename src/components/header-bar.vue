a211147fbc7764693a03bceeb42c4f4b080ccff1
Maha Elleci <maha.elleci@itworx.com>
2021-02-23 21:59:20 +0200

    initialized header bar component - styling only

---
+++ src/components/header-bar.vue
@@ -0,0 +1,30 @@
+<template>  
+<header> 
+    <nav class="main-nav">
+    <div class="nav-bar">  
+    <HeaderSearch></HeaderSearch>  
+       </div> 
+    </nav>
+    </header> 
+</template> 
+
+
+<script>
+import HeaderSearch from "./header-search";
+export default {
+  name: "HeaderBar",
+  components: { HeaderSearch },
+};
+</script>  
+
+<style lang="scss"> 
+.main-nav{ 
+position: relative;
+  .nav-bar {
+   padding: 2rem;
+   background: red;
+   width: 100%;
+  } 
+}
+</style>
+
---
+++ src/components/header-search.vue
@@ -0,0 +1,28 @@
+<template>  
+<div class="search-bar-wrapper">
+    <input class="search-bar" type="text" />  
+     <i class="fas fa-search"></i>
+</div>
+</template> 
+
+<script>
+export default {
+  name: "HeaderSearch",
+};
+</script> 
+
+<style lang="scss" scoped>
+.search-bar-wrapper {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 0.9rem 6.7rem;
+  padding: 0.5rem; 
+  display: flex;  
+  justify-content: space-between;
+  margin-top: 0.9rem;
+  i {
+      color: #fff;
+  }
+}
+</style>
